d671269816b193e1340c5fec4449b7981ffac690
Well <wellsantossilva@gmail.com>
2019-10-14 20:33:24 -0300

    Handle user date input

--- croncatch.py
+++ croncatch.py
@@ -3,11 +3,21 @@ import datetime
 from crontab import CronTab
 
 
+
 cron = CronTab(tabfile='1.tab')
+
+def convert(date_time): 
+    format = '%H:%M'
+    datetime_str = datetime.datetime.strptime(date_time, format) 
+    return datetime_str 
+   
+
+date_time = raw_input("hora: ")
+agora = (convert(date_time)) 
 
 
 for job in cron:
-    sch = job.schedule(date_from=datetime.datetime.now())
+    sch = job.schedule(date_from=agora)
     next_event = sch.get_next()
     ne1 = next_event.strftime('%Y-%m-%d %H:%M')
     ne2 = ne1.split(' ')[1]
@@ -17,11 +27,6 @@ for job in cron:
     t2 = t1.split(' ')[0]
     t3 = datetime.datetime.strftime(next_event,'%Y-%m-%d')
 
-  
-    # print ne2
-    # print "Job: ",job
-    # print "Next Run: ",next_event
-    
     if t2 == t3:
        print ne2," Today - ",job
     else:
