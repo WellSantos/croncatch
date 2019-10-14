b0b6cd96f4707280bf2cfe6e7baf94ffef14d6cd
Well <wellsantossilva@gmail.com>
2019-10-13 23:28:31 -0300

    Testing if today or tomorow

--- croncatch.py
+++ croncatch.py
@@ -8,19 +8,22 @@ cron = CronTab(tabfile='1.tab')
 
 for job in cron:
     sch = job.schedule(date_from=datetime.datetime.now())
-    proximo = sch.get_next()
-    today = datetime.datetime.now()
-    hoje = today.strftime('%Y-%m-%d %H:%M:%S')
-    print "Job: ",job
-    print "Next Run: ",proximo
-    print "Proximo(tipo): ",(type(proximo))
-    print "Hoje: ",hoje
-    print "Today: ",today
-    print "Today(tipo): ",(type(today))
+    next_event = sch.get_next()
+    ne1 = next_event.strftime('%Y-%m-%d %H:%M')
+    ne2 = ne1.split(' ')[1]
     
+    today = datetime.datetime.today()
+    t1 = today.strftime('%Y-%m-%d %H:%M:%S')
+    t2 = t1.split(' ')[0]
+    t3 = datetime.datetime.strftime(next_event,'%Y-%m-%d')
+
+  
+    # print ne2
+    # print "Job: ",job
+    # print "Next Run: ",next_event
     
-    if proximo < today:
-        print "Hoje",today
+    if t2 == t3:
+       print ne2," Today - ",job
     else:
-        print "Amanha",proximo
+        print ne2," Tomorow - ",job
     print "---------------------------------------"
